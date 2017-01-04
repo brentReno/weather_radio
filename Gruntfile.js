3af58d20d2fd8c1642e4b1117c46278d28fe1550
Brent Reno <brent.reno@gmail.com>
2017-01-04 13:23:54 -0600

    Add uglify to grunt

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+     pkg: grunt.file.readJSON('package.json'),
+     uglify: {
+      options: {
+        banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
+      },
+      build: {
+        files: {
+          './server/public/scripts/client.min.js': './server/public/client.js'
+        }
+      }
+    }
+  });
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+};
